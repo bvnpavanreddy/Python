3a4f768013fc9aab3c24b89cc093700dde8534e1
bvnpavanreddy <bvnpavanreddy@gmail.com>
2018-04-19 18:25:03 +0530

    this is for today

---
+++ 04192018/part1.py
@@ -0,0 +1,81 @@
+"""amount =int(input("please enter the amount:"))
+if amount<1000:
+    discount = amount*0.10
+    print("Discount is ",discount)
+
+elif amount<5000:
+    discount = amount*0.20
+    print("Discount is ",discount)
+
+else:
+    discount = amount *0.30
+    print("Discount is ",discount)
+
+print ("net payable" ,amount-discount)"""
+
+"""count = 0
+while count<9:
+    print ("the count is ", count)
+    count = count +1"""
+
+
+"""numbers =[11,21,31,41,51,60,71,81,91,101,201,301]
+for num in numbers:
+    if num%2==0:
+        print("The list contains the even numbers")
+        break
+    else:
+        print ("The list do not have even numbers")"""
+
+"""for i in range(1,21):
+    for k in range(1,21):
+        k=i*k
+        print(k, end=" ")
+    print()#this will print each iteration in the next line"""
+
+"""for letter in 'python':
+    if letter == 'h':
+        print("the current letter is" ,letter)
+        break"""
+
+
+"""var = 15
+while var>0:
+    print ("The current number is :", var)
+    var=var-1
+    if var ==5:
+        break"""
+
+"""import sys
+def fibonacci(n):
+    a,b,counter=0,1,0
+    while True:
+        if (counter>n):
+            return
+        yield a
+        a,b=b,a+b
+        counter += 1
+
+f=fibonacci(15)
+while True:
+    try:
+        print(next(f),end=" ")
+    except StopIteration:
+        sys.exit()"""
+
+
+
+
+            
+    
+    
+
+
+
+    
+    
+
+    
+        
+
+
